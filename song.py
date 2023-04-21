3e93656c08faf24f49df2d16b713185823cc1661
Aditya Dhore <93461722+TechenoHunter@users.noreply.github.com>
2023-04-21 14:18:40 +0530

    Add files via upload

---
+++ song.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+
+window = pygame.display.set_mode((300, 300))
+
+
+pygame.mixer.music.load('E:\instagram reels\all music')
+
+
+pygame.mixer.music.play()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+
+# Stop the music and quit the program
+pygame.mixer
