f522f02f29a1aaa9e3ccf441b99f6c5c6bfaa891
Aditya Dhore <93461722+TechenoHunter@users.noreply.github.com>
2023-04-21 14:23:52 +0530

    Update song.py

--- song.py
+++ song.py
@@ -1,22 +1,13 @@
 import pygame
-
 pygame.init()
-
-
 window = pygame.display.set_mode((300, 300))
-
-
-pygame.mixer.music.load('E:\instagram reels\all music')
-
-
+pygame.mixer.music.load('Insert_music_file_here')
 pygame.mixer.music.play()
-
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             break
-
 # Stop the music and quit the program
 pygame.mixer
